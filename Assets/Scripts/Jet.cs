cf214950d2bff4def69c3c5c76ded9c5cc71e290
Laco1997 <lelkesl1997@gmail.com>
2022-06-17 18:11:39 +0200

    Add a jet

---
+++ Assets/Scripts/Jet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jet : Vehicle
+{
+    public override void Move()
+    {
+        maxMotorTorque = 8f;
+        maxSteeringAngle = 15f;
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+}
